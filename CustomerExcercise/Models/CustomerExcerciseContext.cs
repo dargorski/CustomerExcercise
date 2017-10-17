0d303f75f293c540419ef80588407abdde89328d
dariuszgoorski <dariuszgoorski@gmail.com>
2017-10-17 18:42:12 +0200

    Created CustomersController using Entity Framework

---
+++ CustomerExcercise/Models/CustomerExcerciseContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CustomerExcercise.Models
+{
+    public class CustomerExcerciseContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public CustomerExcerciseContext() : base("name=CustomerExcerciseContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<CustomerExcercise.Models.Customer> Customers { get; set; }
+    }
+}
