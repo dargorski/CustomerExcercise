9fa0770aca50ce05f24de59481005ae988fe79e7
dariuszgoorski <dariuszgoorski@gmail.com>
2017-10-17 18:27:46 +0200

    Added Customer model

---
+++ CustomerExcercise/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CustomerExcercise.Models
+{
+    public class Customer
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public string Number { get; set; }
+        
+        [Required]
+        public string Address { get; set; }
+    }
+}
